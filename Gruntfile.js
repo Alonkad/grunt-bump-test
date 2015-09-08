3966ceb2a65db607ca09ac538ad58b79d9744fcd
Alon Kaduri <alonkad@gmail.com>
2015-09-08 18:41:15 +0300

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+  // Project Configuration
+  grunt.initConfig({
+      bump: {
+          options: {
+              files: ['package.json'],
+              updateConfigs: [],
+              commit: true,
+              commitMessage: 'Release v%VERSION%',
+              commitFiles: ['package.json'],
+              createTag: true,
+              tagName: 'v%VERSION%',
+              tagMessage: 'Version %VERSION%',
+              push: true,
+              pushTo: 'origin', //'upstream',
+              gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+              globalReplace: false,
+              prereleaseName: false,
+              regExp: false
+          }
+      }
+  });
+
+
+  //Default task(s).
+  grunt.registerTask('default', []);
+};
