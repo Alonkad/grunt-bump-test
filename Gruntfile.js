dd27a70fbb32a6dd669177f8976a37b88d6f2ed1
Alon Kaduri <alonkad@gmail.com>
2015-09-09 11:39:21 +0300

    Adding actual version number to change-log

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,40 +4,46 @@ module.exports = function (grunt) {
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
 
-  // Project Configuration
-  grunt.initConfig({
-      bump: {
-          options: {
-              files: ['package.json'],
-              updateConfigs: [],
-              commit: true,
-              commitMessage: 'Release v%VERSION%',
-              commitFiles: ['-a'],
-              createTag: true,
-              tagName: 'v%VERSION%',
-              tagMessage: 'Version %VERSION%',
-              push: true,
-              pushTo: 'origin', //'upstream',
-              gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-              globalReplace: false,
-              prereleaseName: false,
-              regExp: false
-          }
-      },
-      changelog: {
-          release: {
-              options: {
-                  version: 'next'
-              }
-          }
-      }
-  });
+    var getVersionFromPackageJSON = function () {
+        var pJson = grunt.file.readJSON('package.json');
+        return pJson.version;
+    };
 
 
-  //Default task(s).
-  grunt.registerTask('release', [
-      'bump-only:patch',
-      'changelog',
-      'bump-commit'
-  ]);
+    // Project Configuration
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin', //'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
+        },
+        changelog: {
+            release: {
+                options: {
+                    version: getVersionFromPackageJSON()
+                }
+            }
+        }
+    });
+
+
+    //Default task(s).
+    grunt.registerTask('release', [
+        'bump-only:patch',
+        'changelog',
+        'bump-commit'
+    ]);
 };
